0aa07e4b8677270d84e0ca46283405cc4f593069
Moh'd Jaber <moh.ja.ps@gmail.com>
2022-08-02 23:03:02 +0300

    Render Data In Grid #14

--- JS/dom.js
+++ JS/dom.js
@@ -1,3 +1,14 @@
-fetchData('https://api.ipify.org?format=json', (data) => {
-    console.log(data)
-})
+
+const fetchResturants = (countrySet) => {
+    let categorySet = 7315
+    let key = 'Zh7cgV0xS5hBRdNq2lZ8Pdzofe1RwL0w'
+    let url = `https://api.tomtom.com/search/2/search/pizza.json?countrySet=${countrySet}&key=${key}&categorySet=${categorySet}`
+    fetchData(url, (data) => {
+        count.innerText = data.summary.numResults
+        data.results.forEach(place => {
+            renderData(place);
+        })
+    })
+}
+
+fetchResturants('USA')
